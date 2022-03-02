d7b664d3d3de3ccf08e4d2400df2873370c51672
DeeStarks <danielonoja246@gmail.com>
2022-03-02 19:05:12 +0100

    feat: implement 'PopulateUserModel' to help populate one-to-many fields

--- adapters/framework/db/user.go
+++ adapters/framework/db/user.go
@@ -1,21 +1,28 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/deestarks/infiniti/utils"
+	global "github.com/deestarks/infiniti/adapters/framework/db/global"
 )
 
 type (
 	UserModel struct {
-		Id 			int 		`json:"id"`
-		FirstName 	string 		`json:"first_name"`
-		LastName 	string 		`json:"last_name"`
-		Email 		string 		`json:"email"`
-		Password 	string 		`json:"password"`
-		CreatedAt 	time.Time 	`json:"created_at"`
+		Id 					int 					`json:"id"`
+		FirstName 			string 					`json:"first_name"`
+		LastName 			string 					`json:"last_name"`
+		Email 				string 					`json:"email"`
+		Password 			string 					`json:"password"`
+		CreatedAt 			time.Time 				`json:"created_at"`
+		Account 			AccountModel			`json:"account"` // One to One relationship
+		AccountType 		AccountTypeModel		`json:"account_type"` // One to One relationship
+		Currency			CurrencyModel			`json:"currency"` // One to One relationship
+		UserPermissions 	[]UserPermissionsModel	`json:"user_permissions"` // One to Many relationship
+		UserGroups 			[]UserGroupModel		`json:"user_groups"` // One to Many relationship - Many to Many relationship
 	}
 	
 	UserAdapter struct {
@@ -23,6 +30,83 @@ type (
 		tableName	string
 	}
 )
+
+var (
+	selection = `
+		id, first_name, last_name, email, password, created_at,
+		account.id, account.user_id, account.account_type_id, account.account_number, account.balance, account.currency_id,
+		account_type.id, account_type.name,
+		currency.id, currency.name, currency.symbol,
+		permission.id, permission.user_id, permission.permission_id,
+		group.user_id, group.group_id
+	`
+
+	selectionJoin = `
+		OUTER JOIN user_accounts AS account ON account.user_id = users.id
+		OUTER JOIN account_types AS account_type ON account_type.id = account.account_type_id
+		OUTER JOIN currencies AS currency ON currency.id = account.currency_id
+		OUTER JOIN user_permissions AS permission ON permission.user_id = users.id
+		OUTER JOIN user_groups AS group ON group.user_id = users.id
+	`
+)
+
+
+
+func (user *UserModel) PopulateUserModel(row *sql.Rows) error {
+	var (
+		isInitial 		bool // Used to determine if the first row is being populated
+		permission 		UserPermissionsModel
+		userGroup 		UserGroupModel
+	)
+
+	// The following will record ids of foreign keys to avoid duplicates
+	permissionHits 	:= make(global.PreviousIdHits)
+	userGroupHits 	:= make(global.PreviousIdHits)
+
+	for row.Next() {
+		if !isInitial { // If this is the first row, populate user model with the first row
+			isInitial = true
+			if err := row.Scan(
+				&user.Id, &user.FirstName, &user.LastName,
+				&user.Email, &user.Password, &user.CreatedAt,
+				&user.Account.Id, &user.Account.UserId, &user.Account.AccountTypeId, &user.Account.AccountNumber, &user.Account.Balance, &user.Account.CurrencyId,
+				&user.AccountType.Id, &user.AccountType.Name,
+				&user.Currency.Id, &user.Currency.Name, &user.Currency.Symbol,
+				&permission.Id, &permission.UserId, &permission.PermissionId,
+				&userGroup.UserId, &userGroup.GroupId,
+			); err != nil {
+				log.Fatal(err)
+				return err
+			}
+			user.UserPermissions = append(user.UserPermissions, permission)
+			user.UserGroups = append(user.UserGroups, userGroup)
+			permissionHits[permission.Id] = true // Add the permission to the permissionHits map to prevent duplicates
+			userGroupHits[userGroup.GroupId] = true // Add the user group to the userGroupHits map to prevent duplicates
+		} else { // If this is not the first row, populate with only the foreign keys
+			if !permissionHits[permission.Id] { // If the permission has not been hit, add it to the user's permissions
+				if err := row.Scan(
+					&permission.Id, &permission.UserId, &permission.PermissionId,
+				); err != nil {
+					log.Fatal(err)
+					return err
+				}
+				user.UserPermissions = append(user.UserPermissions, permission)
+				permissionHits[permission.Id] = true
+			}
+			if !userGroupHits[userGroup.GroupId] { // If the user group has not been hit, add it to the user's user groups
+				if err := row.Scan(
+					&userGroup.UserId, &userGroup.GroupId,
+				); err != nil {
+					log.Fatal(err)
+					return err
+				}
+				user.UserGroups = append(user.UserGroups, userGroup)
+				userGroupHits[userGroup.GroupId] = true
+			}
+		}
+	}
+	return nil
+}
 
 func (adpt *DBAdapter) NewUserAdapter() *UserAdapter {
 	return &UserAdapter{
@@ -35,13 +119,19 @@ func (adpt *DBAdapter) NewUserAdapter() *UserAdapter {
 func (mAdapt *UserAdapter) Get(col string, value interface{}) (*UserModel, error) {
 	var user UserModel
 	query := fmt.Sprintf(`
-		SELECT id, first_name, last_name, email, password, created_at FROM %s
+		SELECT %s FROM %s
 		WHERE %s = $1
-	`, mAdapt.tableName, col)
-	err := mAdapt.adapter.db.QueryRow(query, value).Scan(
-		&user.Id, &user.FirstName, &user.LastName, 
-		&user.Email, &user.Password, &user.CreatedAt,
-	)
+		%s
+	`, selection, mAdapt.tableName, col, selectionJoin)
+
+	rows, err := mAdapt.adapter.db.Query(query, value)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	err = user.PopulateUserModel(rows)
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
@@ -49,6 +139,7 @@ func (mAdapt *UserAdapter) Get(col string, value interface{}) (*UserModel, error
 	return &user, nil
 }
 
+// TODO: Populate the user model with the user's permissions and user groups
 func (mAdapt *UserAdapter) Filter(col string, value interface{}) (*[]UserModel, error) {
 	var users []UserModel
 	query := fmt.Sprintf(`
@@ -77,6 +168,7 @@ func (mAdapt *UserAdapter) Filter(col string, value interface{}) (*[]UserModel, 
 	return &users, nil
 }
 
+// TODO: Populate the user model with the user's permissions and user groups
 func (mAdapt *UserAdapter) List() (*[]UserModel, error) {
 	var users []UserModel
 	query := fmt.Sprintf(`
