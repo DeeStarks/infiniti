c5c06e4bbb089c4cb445714be2cc9bd5f00685fb
DeeStarks <danielonoja246@gmail.com>
2022-03-02 23:22:02 +0100

    feat: implement 'populateUserModel' to work on multiple rows

--- adapters/framework/db/user.go
+++ adapters/framework/db/user.go
@@ -50,62 +50,127 @@ var (
 	`
 )
 
-
-
-func (user *UserModel) PopulateUserModel(row *sql.Rows) error {
+func populateUserModel(row *sql.Rows, many bool) (interface{}, error) {
 	var (
 		isInitial 		bool // Used to determine if the first row is being populated
 		permission 		UserPermissionsModel
 		userGroup 		UserGroupModel
+		SingleModel		UserModel
+		ManyModels		[]UserModel
 	)
 
 	// The following will record ids of foreign keys to avoid duplicates
+	userHits := make(map[int]bool)
 	permissionHits 	:= make(global.PreviousIdHits)
 	userGroupHits 	:= make(global.PreviousIdHits)
 
 	for row.Next() {
-		if !isInitial { // If this is the first row, populate user model with the first row
-			isInitial = true
-			if err := row.Scan(
-				&user.Id, &user.FirstName, &user.LastName,
-				&user.Email, &user.Password, &user.CreatedAt,
-				&user.Account.Id, &user.Account.UserId, &user.Account.AccountTypeId, &user.Account.AccountNumber, &user.Account.Balance, &user.Account.CurrencyId,
-				&user.AccountType.Id, &user.AccountType.Name,
-				&user.Currency.Id, &user.Currency.Name, &user.Currency.Symbol,
-				&permission.Id, &permission.UserId, &permission.PermissionId,
-				&userGroup.UserId, &userGroup.GroupId,
-			); err != nil {
-				log.Fatal(err)
-				return err
-			}
-			user.UserPermissions = append(user.UserPermissions, permission)
-			user.UserGroups = append(user.UserGroups, userGroup)
-			permissionHits[permission.Id] = true // Add the permission to the permissionHits map to prevent duplicates
-			userGroupHits[userGroup.GroupId] = true // Add the user group to the userGroupHits map to prevent duplicates
-		} else { // If this is not the first row, populate with only the foreign keys
-			if !permissionHits[permission.Id] { // If the permission has not been hit, add it to the user's permissions
+		var userId int
+		err := row.Scan(&userId)
+		if err != nil {
+			log.Fatal(err)
+			return nil, err
+		}
+
+		if !many {
+			if !isInitial { // If this is the first row, populate user model with the first row
+				isInitial = true
 				if err := row.Scan(
+					&SingleModel.Id, &SingleModel.FirstName, &SingleModel.LastName,
+					&SingleModel.Email, &SingleModel.Password, &SingleModel.CreatedAt,
+					&SingleModel.Account.Id, &SingleModel.Account.UserId, &SingleModel.Account.AccountTypeId, 
+					&SingleModel.Account.AccountNumber, &SingleModel.Account.Balance, &SingleModel.Account.CurrencyId,
+					&SingleModel.AccountType.Id, &SingleModel.AccountType.Name,
+					&SingleModel.Currency.Id, &SingleModel.Currency.Name, &SingleModel.Currency.Symbol,
 					&permission.Id, &permission.UserId, &permission.PermissionId,
-				); err != nil {
-					log.Fatal(err)
-					return err
-				}
-				user.UserPermissions = append(user.UserPermissions, permission)
-				permissionHits[permission.Id] = true
-			}
-			if !userGroupHits[userGroup.GroupId] { // If the user group has not been hit, add it to the user's user groups
-				if err := row.Scan(
 					&userGroup.UserId, &userGroup.GroupId,
 				); err != nil {
 					log.Fatal(err)
-					return err
+					return nil, err
 				}
-				user.UserGroups = append(user.UserGroups, userGroup)
-				userGroupHits[userGroup.GroupId] = true
+				SingleModel.UserPermissions = append(SingleModel.UserPermissions, permission)
+				SingleModel.UserGroups = append(SingleModel.UserGroups, userGroup)
+				permissionHits[permission.Id] = true // Add the permission to the permissionHits map to prevent duplicates
+				userGroupHits[userGroup.GroupId] = true // Add the user group to the userGroupHits map to prevent duplicates
+			} else { // If this is not the first row, populate with only the foreign keys
+				if !permissionHits[permission.Id] { // If the permission has not been hit, add it to the user's permissions
+					if err := row.Scan(
+						&permission.Id, &permission.UserId, &permission.PermissionId,
+					); err != nil {
+						log.Fatal(err)
+						return nil, err
+					}
+					SingleModel.UserPermissions = append(SingleModel.UserPermissions, permission)
+					permissionHits[permission.Id] = true
+				}
+				if !userGroupHits[userGroup.GroupId] { // If the user group has not been hit, add it to the user's user groups
+					if err := row.Scan(
+						&userGroup.UserId, &userGroup.GroupId,
+					); err != nil {
+						log.Fatal(err)
+						return nil, err
+					}
+					SingleModel.UserGroups = append(SingleModel.UserGroups, userGroup)
+					userGroupHits[userGroup.GroupId] = true
+				}
+			}
+		} else {
+			if !userHits[userId] && SingleModel.Id != 0 { // If the user has not been hit and the user model has been populated
+				ManyModels = append(ManyModels, SingleModel) // Add the user model to the list of user models
+				SingleModel = UserModel{} // Reset the user model
+				permissionHits = make(global.PreviousIdHits) // Reset the permission hits
+				userGroupHits = make(global.PreviousIdHits) // Reset the user group hits
+				isInitial = false
+			}
+
+			if !isInitial { // If this is the first row, populate user model with the first row
+				isInitial = true
+				if err := row.Scan(
+					&SingleModel.Id, &SingleModel.FirstName, &SingleModel.LastName,
+					&SingleModel.Email, &SingleModel.Password, &SingleModel.CreatedAt,
+					&SingleModel.Account.Id, &SingleModel.Account.UserId, &SingleModel.Account.AccountTypeId, 
+					&SingleModel.Account.AccountNumber, &SingleModel.Account.Balance, &SingleModel.Account.CurrencyId,
+					&SingleModel.AccountType.Id, &SingleModel.AccountType.Name,
+					&SingleModel.Currency.Id, &SingleModel.Currency.Name, &SingleModel.Currency.Symbol,
+					&permission.Id, &permission.UserId, &permission.PermissionId,
+					&userGroup.UserId, &userGroup.GroupId,
+				); err != nil {
+					log.Fatal(err)
+					return nil, err
+				}
+				SingleModel.UserPermissions = append(SingleModel.UserPermissions, permission)
+				SingleModel.UserGroups = append(SingleModel.UserGroups, userGroup)
+				permissionHits[permission.Id] = true // Add the permission to the permissionHits map to prevent duplicates
+				userGroupHits[userGroup.GroupId] = true // Add the user group to the userGroupHits map to prevent duplicates
+				userHits[userId] = true // Add the user to the userHits map to prevent duplicates
+			} else { // If this is not the first row, populate with only the foreign keys
+				if !permissionHits[permission.Id] { // If the permission has not been hit, add it to the user's permissions
+					if err := row.Scan(
+						&permission.Id, &permission.UserId, &permission.PermissionId,
+					); err != nil {
+						log.Fatal(err)
+						return nil, err
+					}
+					SingleModel.UserPermissions = append(SingleModel.UserPermissions, permission)
+					permissionHits[permission.Id] = true
+				}
+				if !userGroupHits[userGroup.GroupId] { // If the user group has not been hit, add it to the user's user groups
+					if err := row.Scan(
+						&userGroup.UserId, &userGroup.GroupId,
+					); err != nil {
+						log.Fatal(err)
+						return nil, err
+					}
+					SingleModel.UserGroups = append(SingleModel.UserGroups, userGroup)
+					userGroupHits[userGroup.GroupId] = true
+				}
 			}
 		}
 	}
-	return nil
+	if many {
+		return &ManyModels, nil
+	}
+	return &SingleModel, nil
 }
 
 func (adpt *DBAdapter) NewUserAdapter() *UserAdapter {
@@ -117,7 +182,6 @@ func (adpt *DBAdapter) NewUserAdapter() *UserAdapter {
 
 // Define the methods of the UserAdapter
 func (mAdapt *UserAdapter) Get(col string, value interface{}) (*UserModel, error) {
-	var user UserModel
 	query := fmt.Sprintf(`
 		SELECT %s FROM %s
 		WHERE %s = $1
@@ -131,20 +195,21 @@ func (mAdapt *UserAdapter) Get(col string, value interface{}) (*UserModel, error
 	}
 	defer rows.Close()
 
-	err = user.PopulateUserModel(rows)
+	arrangedUser, err := populateUserModel(rows, false)
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
 	}
-	return &user, nil
+
+	user := arrangedUser.(*UserModel)
+	return user, nil
 }
 
 // TODO: Populate the user model with the user's permissions and user groups
 func (mAdapt *UserAdapter) Filter(col string, value interface{}) (*[]UserModel, error) {
-	var users []UserModel
 	query := fmt.Sprintf(`
 		SELECT id, first_name, last_name, email, password, created_at FROM %s
-		WHERE %s = $1
+		WHERE %s = $1 ORDER BY id ASC
 	`, mAdapt.tableName, col)
 	rows, err := mAdapt.adapter.db.Query(query, value)
 	if err != nil {
@@ -153,26 +218,21 @@ func (mAdapt *UserAdapter) Filter(col string, value interface{}) (*[]UserModel, 
 	}
 	defer rows.Close()
 
-	for rows.Next() {
-		var user UserModel
-		err := rows.Scan(
-			&user.Id, &user.FirstName, &user.LastName,
-			&user.Email, &user.Password, &user.CreatedAt,
-		)
-		if err != nil {
-			log.Fatal(err)
-			return nil, err
-		}
-		users = append(users, user)
+	arrangedUsers, err := populateUserModel(rows, true)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
 	}
-	return &users, nil
+
+	users := arrangedUsers.(*[]UserModel)
+	return users, nil
 }
 
 // TODO: Populate the user model with the user's permissions and user groups
 func (mAdapt *UserAdapter) List() (*[]UserModel, error) {
-	var users []UserModel
 	query := fmt.Sprintf(`
-		SELECT id, first_name, last_name, email, password, created_at FROM %s
+		SELECT id, first_name, last_name, email, password, created_at 
+		FROM %s ORDER BY id ASC
 	`, mAdapt.tableName)
 	rows, err := mAdapt.adapter.db.Query(query)
 	if err != nil {
@@ -180,19 +240,15 @@ func (mAdapt *UserAdapter) List() (*[]UserModel, error) {
 		return nil, err
 	}
 	defer rows.Close()
-	for rows.Next() {
-		var user UserModel
-		err := rows.Scan(
-			&user.Id, &user.FirstName, &user.LastName,
-			&user.Email, &user.Password, &user.CreatedAt,
-		)
-		if err != nil {
-			log.Fatal(err)
-			return nil, err
-		}
-		users = append(users, user)
+	
+	arrangedUsers, err := populateUserModel(rows, true)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
 	}
-	return &users, nil
+
+	users := arrangedUsers.(*[]UserModel)
+	return users, nil
 }
 
 func (mAdapt *UserAdapter) Create(data map[string]interface{}) (*UserModel, error) {
