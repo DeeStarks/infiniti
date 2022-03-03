b4ef4687e794094b311c7bf433ce0385ced5e67e
DeeStarks <danielonoja246@gmail.com>
2022-03-03 14:56:09 +0100

    feat: complete implementation for single user GET's arrangement into single structured model

--- adapters/framework/db/user.go
+++ adapters/framework/db/user.go
@@ -33,26 +33,26 @@ type (
 
 var (
 	selection = `
-		id, first_name, last_name, email, password, created_at,
-		account.id, account.user_id, account.account_type_id, account.account_number, account.balance, account.currency_id,
-		account_type.id, account_type.name,
-		currency.id, currency.name, currency.symbol,
-		permission.id, permission.user_id, permission.permission_id,
-		group.user_id, group.group_id
+		users.id, users.first_name, users.last_name, users.email, users.password, users.created_at,
+		COALESCE(account.id, 0), COALESCE(account.user_id, 0), COALESCE(account.account_type_id, 0), COALESCE(account.account_number, 0), COALESCE(account.balance, 0.0), COALESCE(account.currency_id, 0),
+		COALESCE(account_type.id, 0), COALESCE(account_type.name, ''),
+		COALESCE(currency.id, 0), COALESCE(currency.name, ''), COALESCE(currency.symbol, ''),
+		COALESCE(permission.id, 0), COALESCE(permission.user_id, 0), COALESCE(permission.permission_id, 0),
+		COALESCE(user_group.user_id, 0), COALESCE(user_group.group_id, 0)
 	`
 
 	selectionJoin = `
-		OUTER JOIN user_accounts AS account ON account.user_id = users.id
-		OUTER JOIN account_types AS account_type ON account_type.id = account.account_type_id
-		OUTER JOIN currencies AS currency ON currency.id = account.currency_id
-		OUTER JOIN user_permissions AS permission ON permission.user_id = users.id
-		OUTER JOIN user_groups AS group ON group.user_id = users.id
+		LEFT OUTER JOIN user_accounts AS account ON account.user_id = users.id
+		LEFT OUTER JOIN account_types AS account_type ON account_type.id = account.account_type_id
+		LEFT OUTER JOIN currencies AS currency ON currency.id = account.currency_id
+		LEFT OUTER JOIN user_permissions AS permission ON permission.user_id = users.id
+		LEFT OUTER JOIN user_groups AS user_group ON user_group.user_id = users.id
 	`
 )
 
 func populateUserModel(row *sql.Rows, many bool) (interface{}, error) {
 	var (
-		isInitial 		bool // Used to determine if the first row is being populated
+		// notInitial 		bool // Used to determine if the first row is being populated
 		permission 		UserPermissionsModel
 		userGroup 		UserGroupModel
 		SingleModel		UserModel
@@ -65,104 +65,59 @@ func populateUserModel(row *sql.Rows, many bool) (interface{}, error) {
 	userGroupHits 	:= make(global.PreviousIdHits)
 
 	for row.Next() {
-		var userId int
-		err := row.Scan(&userId)
-		if err != nil {
+		if err := row.Scan(
+			&SingleModel.Id, &SingleModel.FirstName, &SingleModel.LastName,
+			&SingleModel.Email, &SingleModel.Password, &SingleModel.CreatedAt,
+			&SingleModel.Account.Id, &SingleModel.Account.UserId, &SingleModel.Account.AccountTypeId, 
+			&SingleModel.Account.AccountNumber, &SingleModel.Account.Balance, &SingleModel.Account.CurrencyId,
+			&SingleModel.AccountType.Id, &SingleModel.AccountType.Name,
+			&SingleModel.Currency.Id, &SingleModel.Currency.Name, &SingleModel.Currency.Symbol,
+			&permission.Id, &permission.UserId, &permission.PermissionId,
+			&userGroup.UserId, &userGroup.GroupId,
+		); err != nil {
 			log.Fatal(err)
 			return nil, err
 		}
 
 		if !many {
-			if !isInitial { // If this is the first row, populate user model with the first row
-				isInitial = true
-				if err := row.Scan(
-					&SingleModel.Id, &SingleModel.FirstName, &SingleModel.LastName,
-					&SingleModel.Email, &SingleModel.Password, &SingleModel.CreatedAt,
-					&SingleModel.Account.Id, &SingleModel.Account.UserId, &SingleModel.Account.AccountTypeId, 
-					&SingleModel.Account.AccountNumber, &SingleModel.Account.Balance, &SingleModel.Account.CurrencyId,
-					&SingleModel.AccountType.Id, &SingleModel.AccountType.Name,
-					&SingleModel.Currency.Id, &SingleModel.Currency.Name, &SingleModel.Currency.Symbol,
-					&permission.Id, &permission.UserId, &permission.PermissionId,
-					&userGroup.UserId, &userGroup.GroupId,
-				); err != nil {
-					log.Fatal(err)
-					return nil, err
-				}
+			if !permissionHits[permission.Id] { // If the permission has not been hit, add it to the user's permissions
 				SingleModel.UserPermissions = append(SingleModel.UserPermissions, permission)
+				permissionHits[permission.Id] = true
+			}
+			if !userGroupHits[userGroup.GroupId] { // If the user group has not been hit, add it to the user's user groups
 				SingleModel.UserGroups = append(SingleModel.UserGroups, userGroup)
-				permissionHits[permission.Id] = true // Add the permission to the permissionHits map to prevent duplicates
-				userGroupHits[userGroup.GroupId] = true // Add the user group to the userGroupHits map to prevent duplicates
-			} else { // If this is not the first row, populate with only the foreign keys
-				if !permissionHits[permission.Id] { // If the permission has not been hit, add it to the user's permissions
-					if err := row.Scan(
-						&permission.Id, &permission.UserId, &permission.PermissionId,
-					); err != nil {
-						log.Fatal(err)
-						return nil, err
-					}
-					SingleModel.UserPermissions = append(SingleModel.UserPermissions, permission)
-					permissionHits[permission.Id] = true
-				}
-				if !userGroupHits[userGroup.GroupId] { // If the user group has not been hit, add it to the user's user groups
-					if err := row.Scan(
-						&userGroup.UserId, &userGroup.GroupId,
-					); err != nil {
-						log.Fatal(err)
-						return nil, err
-					}
-					SingleModel.UserGroups = append(SingleModel.UserGroups, userGroup)
-					userGroupHits[userGroup.GroupId] = true
-				}
+				userGroupHits[userGroup.GroupId] = true
 			}
 		} else {
-			if !userHits[userId] { // If the user has not been hit
+			// <=========== TODO: Complete this section ===========>
+
+			if !userHits[SingleModel.Id] { // If the user has not been hit
 				if SingleModel.Id != 0 { // If the user model has been populated
+					fmt.Println(SingleModel.Id)
 					ManyModels 		= append(ManyModels, SingleModel) // Add the user model to the list of user models
 					SingleModel 	= UserModel{} // Reset the user model
 					permissionHits 	= make(global.PreviousIdHits) // Reset the permission hits
 					userGroupHits 	= make(global.PreviousIdHits) // Reset the user group hits
 				}
 
-				if err := row.Scan(
-					&SingleModel.Id, &SingleModel.FirstName, &SingleModel.LastName,
-					&SingleModel.Email, &SingleModel.Password, &SingleModel.CreatedAt,
-					&SingleModel.Account.Id, &SingleModel.Account.UserId, &SingleModel.Account.AccountTypeId, 
-					&SingleModel.Account.AccountNumber, &SingleModel.Account.Balance, &SingleModel.Account.CurrencyId,
-					&SingleModel.AccountType.Id, &SingleModel.AccountType.Name,
-					&SingleModel.Currency.Id, &SingleModel.Currency.Name, &SingleModel.Currency.Symbol,
-					&permission.Id, &permission.UserId, &permission.PermissionId,
-					&userGroup.UserId, &userGroup.GroupId,
-				); err != nil {
-					log.Fatal(err)
-					return nil, err
-				}
 				SingleModel.UserPermissions = append(SingleModel.UserPermissions, permission)
 				SingleModel.UserGroups = append(SingleModel.UserGroups, userGroup)
 				permissionHits[permission.Id] = true // Add the permission to the permissionHits map to prevent duplicates
 				userGroupHits[userGroup.GroupId] = true // Add the user group to the userGroupHits map to prevent duplicates
-				userHits[userId] = true // Add the user to the userHits map to prevent duplicates
+				userHits[SingleModel.Id] = true // Add the user to the userHits map to prevent duplicates
+				fmt.Println(userHits[SingleModel.Id])
 			} else { // If this is not the first row, populate with only the foreign keys
 				if !permissionHits[permission.Id] { // If the permission has not been hit, add it to the user's permissions
-					if err := row.Scan(
-						&permission.Id, &permission.UserId, &permission.PermissionId,
-					); err != nil {
-						log.Fatal(err)
-						return nil, err
-					}
 					SingleModel.UserPermissions = append(SingleModel.UserPermissions, permission)
 					permissionHits[permission.Id] = true
 				}
 				if !userGroupHits[userGroup.GroupId] { // If the user group has not been hit, add it to the user's user groups
-					if err := row.Scan(
-						&userGroup.UserId, &userGroup.GroupId,
-					); err != nil {
-						log.Fatal(err)
-						return nil, err
-					}
 					SingleModel.UserGroups = append(SingleModel.UserGroups, userGroup)
 					userGroupHits[userGroup.GroupId] = true
 				}
 			}
+
+			// <========================================>
 		}
 	}
 	if many {
@@ -185,9 +140,9 @@ func (adpt *DBAdapter) NewUserAdapter() *UserAdapter {
 func (mAdapt *UserAdapter) Get(col string, value interface{}) (*UserModel, error) {
 	query := fmt.Sprintf(`
 		SELECT %s FROM %s
-		WHERE %s = $1
 		%s
-	`, selection, mAdapt.tableName, col, selectionJoin)
+		WHERE users.%s = $1
+	`, selection, mAdapt.tableName, selectionJoin, col)
 
 	rows, err := mAdapt.adapter.db.Query(query, value)
 	if err != nil {
@@ -232,9 +187,10 @@ func (mAdapt *UserAdapter) Filter(col string, value interface{}) (*[]UserModel, 
 // TODO: Populate the user model with the user's permissions and user groups
 func (mAdapt *UserAdapter) List() (*[]UserModel, error) {
 	query := fmt.Sprintf(`
-		SELECT id, first_name, last_name, email, password, created_at 
-		FROM %s ORDER BY id ASC
-	`, mAdapt.tableName)
+		SELECT %s FROM %s
+		%s
+		ORDER BY id ASC
+	`, selection, mAdapt.tableName, selectionJoin)
 	rows, err := mAdapt.adapter.db.Query(query)
 	if err != nil {
 		log.Fatal(err)
