736966d886f085751d17b0dc46a7130080df00f1
DeeStarks <danielonoja246@gmail.com>
2022-03-22 01:39:20 +0100

    feat: create db and app utils

---
+++ utils/app.utils.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+    "encoding/json"
+)
+
+// The struct - "obj", fields must have json tags
+func StructToMap(obj interface{}) map[string]interface{} {
+	b, _ := json.Marshal(obj)
+	var result map[string]interface{}
+	json.Unmarshal(b, &result)
+	return result
+}
