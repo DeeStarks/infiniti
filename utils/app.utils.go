7fce072df5690e23e4d1c35867d6fc2763d7853a
DeeStarks <danielonoja246@gmail.com>
2022-03-24 20:08:36 +0100

    feat: implement struct slice to map slice type util function

--- utils/app.utils.go
+++ utils/app.utils.go
@@ -11,3 +11,10 @@ func StructToMap(obj interface{}) map[string]interface{} {
 	json.Unmarshal(b, &result)
 	return result
 }
+
+func StructSliceToMap(obj interface{}) []map[string]interface{} {
+	b, _ := json.Marshal(obj)
+	var result []map[string]interface{}
+	json.Unmarshal(b, &result)
+	return result
+}
