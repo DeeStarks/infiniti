005c8d3841953e8e254e1b64b95fea41cde45e15
DeeStarks <danielonoja246@gmail.com>
2022-04-02 17:22:47 +0100

    feat: send services related error messages directly from app layer

--- application/services/account.go
+++ application/services/account.go
@@ -1,10 +1,12 @@
 package services
 
 import (
+	"net/http"
+
 	"github.com/deestarks/infiniti/adapters/framework/db"
 	"github.com/deestarks/infiniti/application/core"
-	"github.com/deestarks/infiniti/utils"
 	"github.com/deestarks/infiniti/application/services/constants"
+	"github.com/deestarks/infiniti/utils"
 )
 
 type Account struct {
@@ -24,22 +26,22 @@ func (service *Service) NewAccountService() *Account {
 // Key -> Column to get data from
 // Value -> Value to match
 // Includes -> Account related columns to include in the result
-func (account *Account) GetAccount(key string, value interface{}, includes []string) (constants.ServiceStructReturnType, error) {
+func (account *Account) GetAccount(key string, value interface{}, includes []string) (constants.ServiceStructReturnType, utils.AppError) {
 	accountAdapter := account.dbPort.NewAccountAdapter()
 	acct, err := accountAdapter.Get(key, value)
 	if err != nil {
-		return nil, err
+		return nil, utils.NewAppError("Account not found", http.StatusNotFound)
 	}
 
 	serializedAcct := constants.ServiceStructReturnType(utils.StructToMap(acct))
 	return serializedAcct, nil
 }
 
-func (account *Account) ListAccounts() (constants.ServiceStructSliceReturnType, error) {
+func (account *Account) ListAccounts() (constants.ServiceStructSliceReturnType, utils.AppError) {
 	accountAdapter := account.dbPort.NewAccountAdapter()
 	accts, err := accountAdapter.List()
 	if err != nil {
-		return nil, err
+		return nil, utils.NewAppError(err.Error(), http.StatusInternalServerError)
 	}
 
 	serializedAccts := constants.ServiceStructSliceReturnType(utils.StructSliceToMap(accts))
@@ -49,11 +51,11 @@ func (account *Account) ListAccounts() (constants.ServiceStructSliceReturnType, 
 
 
 // ACCOUNT TYPES
-func (account *Account) GetAccountType(key string, value interface{}) (constants.ServiceStructReturnType, error) {
+func (account *Account) GetAccountType(key string, value interface{}) (constants.ServiceStructReturnType, utils.AppError) {
 	accountTypeAdapter := account.dbPort.NewAccountTypeAdapter()
 	acctType, err := accountTypeAdapter.Get(key, value)
 	if err != nil {
-		return nil, err
+		return nil, utils.NewAppError("Account type not found", http.StatusNotFound)
 	}
 
 	serializedAcctType := constants.ServiceStructReturnType(utils.StructToMap(acctType))
